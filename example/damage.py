3db807ab484f92beb8b98f7a8a44a5f4527018de
Pedro Cattori <pcattori@gmail.com>
2017-01-09 20:27:03 -0500

    clarifying comments for gen1 damage example

--- example/damage.py
+++ example/damage.py
@@ -1,14 +1,13 @@
 import pokemon
+# NOTE: ivs are generated as if these pokemon were wild
+# NOTE: evs are zeroed out
+# NOTE: damage formula is non-deterministic so your results may vary
 charmander = pokemon.Pokemon(pokemon.species('charmander'), level=3, moves=[
     pokemon.move(move) for move in ('scratch', 'growl', 'ember', 'leer')])
-# print('species', charmander.species)
-# print('level', charmander.level)
-# print('ivs', charmander.ivs)
-# print('evs', charmander.evs)
-# print('stats', charmander.stats)
-# print('moves', charmander.moves)
+
 squirtle = pokemon.Pokemon(pokemon.species('squirtle'), level=3, moves=[
     pokemon.move(move) for move in ('tackle', 'tail whip', 'bubble', 'water gun')])
 
 dmg = pokemon.formulas.damage(squirtle, squirtle.moves[2], charmander)
 print(dmg)
+# example: prints => Damage(damage=13, luck=0.9900914399158687, critical_hit=False, effectiveness=2)
