7bab9d7ebdb99691105394bf35d7c5b9187767af
Pedro Cattori <pcattori@gmail.com>
2017-01-09 19:06:37 -0500

    explicit importing

--- pokemon/__init__.py
+++ pokemon/__init__.py
@@ -1,4 +1,4 @@
-from pokemon.core import *
+from pokemon.core import Move, Species, Stats, TypeEffectiveness
 from pokemon.pokedex import MOVEDEX, POKEDEX
 from pokemon.pokemon import Pokemon
-from pokemon.utils import *
+from pokemon.utils import move, species
