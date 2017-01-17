92a1311e38591194a0197166f3a9cced9a84afbf
Pedro Cattori <pcattori@gmail.com>
2017-01-16 20:37:49 -0500

    updated import paths in __init__

--- pokemon/__init__.py
+++ pokemon/__init__.py
@@ -1,4 +1,4 @@
-from pokemon.core import Move, Species, Stats, TypeEffectiveness
-from pokemon.pokedex import MOVEDEX, POKEDEX
+from pokemon.core import move, move_effect, Move, Species, Stats, TypeEffectiveness
+from pokemon.data import MOVEDEX, POKEDEX, TYPE_CHART
 from pokemon.pokemon import Pokemon
-from pokemon.utils import move, species
+from pokemon.utils import find_move, find_species
