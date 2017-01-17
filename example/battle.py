7d9ffd6d7516fb3036a8342d75d0d09d60cf1e0a
Pedro Cattori <pcattori@gmail.com>
2017-01-16 20:42:31 -0500

    updated examples

--- example/battle.py
+++ example/battle.py
@@ -3,36 +3,36 @@ import pokemon.battle
 
 if __name__ == '__main__':
     red_team = pokemon.battle.Team([
-        pokemon.Pokemon(pokemon.species('charmander'), level=3, moves=[
-            pokemon.move(move) for move in (
+        pokemon.Pokemon(pokemon.find_species('charmander'), level=3, moves=[
+            pokemon.find_move(move) for move in (
                 'scratch', 'growl', 'ember', 'leer')])])
     blue_team = pokemon.battle.Team([
-        pokemon.Pokemon(pokemon.species('squirtle'), level=3, moves=[
-            pokemon.move(move) for move in (
+        pokemon.Pokemon(pokemon.find_species('squirtle'), level=3, moves=[
+            pokemon.find_move(move) for move in (
                 'tackle', 'tail whip', 'bubble', 'water gun')])])
     teams = (red_team, blue_team)
 
     while all(not team.blacked_out() for team in teams):
         for team in teams:
             print(f'{team.fighter.name} has {team.fighter.hp} hp')
-        fight_actions = []
+        move_choices = []
         for i, team in enumerate(teams):
             other_team = teams[(i + 1) % 2]
             print(f'What will {team.fighter.name} do?')
             print(*list(team.fighter.moves.keys()), sep='\n')
             move_name = input('>> ')
-            fight_actions.append(pokemon.battle.FightAction(
-                pokemon=team.fighter, move=move_name, target=other_team.fighter))
-        for result in pokemon.battle.fight(fight_actions):
-            print(f'{result.action.pokemon.name} used {result.action.move}!')
+            move_choices.append(pokemon.battle.MoveChoice(
+                fighter=team.fighter, move=move_name, target=other_team.fighter))
+        for result in pokemon.battle.fight(move_choices):
+            print(f'{result.move_choice.fighter.name} used {result.move_choice.move}!')
             if result.miss:
-                print(f'{result.action.move} missed!')
+                print(f'{result.move_choice.move} missed!')
                 continue
             if result.damage.critical_hit:
                 print('Critical hit!')
             print(f'{result.damage.effectiveness}x effective')
             if result.ko:
-                print(f'{result.action.target.name} fainted!')
+                print(f'{result.move_choice.target.name} fainted!')
             else:
-                print(f'{result.action.target.name} now has {result.action.target.hp} hp')
+                print(f'{result.move_choice.target.name} now has {result.move_choice.target.hp} hp')
         print('\n==================\n')
